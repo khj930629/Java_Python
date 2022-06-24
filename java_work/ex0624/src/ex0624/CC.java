aa07b54ceafc4fd8952fddfc5e70319922c362bc
khj930629 <kwon-_-hj@naver.com>
2022-06-24 10:49:15 +0900

    20220624

---
+++ java_work/ex0624/src/ex0624/CC.java
@@ -0,0 +1,15 @@
+package ex0624;
+
+public class CC {
+
+	public static void main(String[] args) {
+
+		short a = 32767;
+		short b = 20;
+		
+		System.out.println(" a + b ="+(a+b));
+
+		short c = (short) (a+b);
+		System.out.println(" c ="+c);
+	}
+}
---
+++ java_work/ex0624/src/ex0624/DD.java
@@ -0,0 +1,22 @@
+package ex0624;
+
+public class DD {
+
+	public static void main(String[] args) {
+		
+		final int AA = 10;
+		final String CC = "문자";
+		
+		System.out.println("AA = "+ AA);
+		System.out.println("CC = "+ CC);
+		
+		System.out.println("안녕하세요\n");
+		System.out.println("안녕하세요\t권혁준입니다.");
+		System.out.println("안녕하세요");
+		System.out.println("\'안녕하세요\'");
+		System.out.println("\\안녕하세요\\");
+
+//		AA = 20;
+		
+	}
+}
