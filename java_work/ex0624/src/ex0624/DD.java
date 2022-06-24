06a25dc93c0baa6de384b60c399debf0b7a3179a
khj930629 <kwon-_-hj@naver.com>
2022-06-24 11:53:15 +0900

    vscode

--- java_work/ex0624/src/ex0624/DD.java
+++ java_work/ex0624/src/ex0624/DD.java
@@ -17,6 +17,17 @@ public class DD {
 		System.out.println("\\안녕하세요\\");
 
 //		AA = 20;
+		/*
+		 * 한번만...
+		 * git init 현재폴더를 git으로 초기화
+		 * git config email.name
+		 * git remote add 주소
+		 * 
+		 * 할때마다...
+		 * git add . 현재 폴더 밑에 있는 변경된 파일 업로드
+		 * git commit -m "20" 메시지 남기고 승인
+		 * git push origin main 서버로 업로드 
+		 */
 		
 	}
 }
---
+++ java_work/ex0624/src/ex0624/EE.java
@@ -0,0 +1,16 @@
+package ex0624;
+
+public class EE {
+
+	public static void main(String[] args) {
+		
+		double pi = 3.14;
+		int pia = (int) pi;
+		
+		System.out.println("pia = "+pia);
+		
+		double a = 3.123123d;
+		int pib = (int)a;
+		System.out.println("(int)a = "+pib);
+	}
+}
