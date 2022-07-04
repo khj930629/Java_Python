2dee339628f26abdd0db15cac2b55a9b8b3b218f
khj930629 <kwon-_-hj@naver.com>
2022-07-04 14:38:59 +0900

    220704

---
+++ python_work/220704/ex08.py
@@ -0,0 +1,16 @@
+a = "a,b,c,d,e,f,o"
+
+a = a.replace("a", "haha")
+print(a)
+
+lista = a.split(',')
+print(lista)
+
+for i in lista:
+    print('i', i)
+
+'''
+    git add .
+    git commit -m "220704"
+    git push origin main
+'''
